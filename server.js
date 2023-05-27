a43fcad58a7f72495d3f956a5c33ef00fd4ec789
anza04 <64538739+anza04@users.noreply.github.com>
2023-05-27 08:30:34 +0200

    md5 + SQL api commente

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+
+const MD5 = require('crypto-js/md5.js');
+
 const port = process.env.PORT || 8383;
 const {dbFirebase} = require('./firebase.js');
 const {dbMysql} = require('./mysql.js');
@@ -26,86 +29,86 @@ app.post('/aggiungiTenda', async (req, res) =>{
     const {username, pw} = req.body;
     const utentiRef = dbFirebase.collection("utenti").doc("utente");
     const res2 = await utentiRef.set({
-        [username]: pw
+        [username]: MD5(pw)
     }, {merge:true})
 })
 
-/*  API GENERALI    */
+// /*  API GENERALI    */
 
-app.get("/accesso/:criterio/:valore", (req, res)=>{
-    dbMysql.connect();
-    const stringa = "SELECT credenziali_utente.pw, credenziali_utente.IdVolontarioPk  FROM credenziali_utente WHERE "+ req.params.criterio + "='"+req.params.valore+"'";
-    dbMysql.query(stringa, (err,result)=>{
+// app.get("/accesso/:criterio/:valore", (req, res)=>{
+//     dbMysql.connect();
+//     const stringa = "SELECT credenziali_utente.pw, credenziali_utente.IdVolontarioPk  FROM credenziali_utente WHERE "+ req.params.criterio + "='"+req.params.valore+"'";
+//     dbMysql.query(stringa, (err,result)=>{
 
-        if (err) {console.log(err)}
-        res.send(result);
-    });
-    dbMysql.end();
-})
+//         if (err) {console.log(err)}
+//         res.send(result);
+//     });
+//     dbMysql.end();
+// })
 
-app.get("/stampaVolontario/:criterio/:valore", async(req, res)=>{
-    dbMysql.connect();
-    const stringa = "SELECT * FROM anagrafica_volontari WHERE "+ req.params.criterio + "='"+req.params.valore+"'";
-    dbMysql.query(stringa, (err,result)=>{
+// app.get("/stampaVolontario/:criterio/:valore", async(req, res)=>{
+//     dbMysql.connect();
+//     const stringa = "SELECT * FROM anagrafica_volontari WHERE "+ req.params.criterio + "='"+req.params.valore+"'";
+//     dbMysql.query(stringa, (err,result)=>{
 
-        if (err) {console.log(err)}
-        else res.send(result);
-    });
-    dbMysql.end();
-})
+//         if (err) {console.log(err)}
+//         else res.send(result);
+//     });
+//     dbMysql.end();
+// })
 
-app.get("/stampaVolontari", (req, res)=>{
-    dbMysql.connect();
-    const stringa = "SELECT * FROM anagrafica_volontari";
-    dbMysql.query(stringa, (err,result)=>{
-        if (err) {console.log(err)}
-        res.send(result);
-    });
-    dbMysql.end();
-})
+// app.get("/stampaVolontari", (req, res)=>{
+//     dbMysql.connect();
+//     const stringa = "SELECT * FROM anagrafica_volontari";
+//     dbMysql.query(stringa, (err,result)=>{
+//         if (err) {console.log(err)}
+//         res.send(result);
+//     });
+//     dbMysql.end();
+// })
 
-app.delete("/eliminaVolontario/:criterio/:valore", (req, res)=>{
-    dbMysql.connect();
-    const stringa = "DELETE FROM anagrafica_volontari WHERE "+ req.params.criterio + "='"+req.params.valore+"'";
-    dbMysql.query(stringa, (err,result)=>{
+// app.delete("/eliminaVolontario/:criterio/:valore", (req, res)=>{
+//     dbMysql.connect();
+//     const stringa = "DELETE FROM anagrafica_volontari WHERE "+ req.params.criterio + "='"+req.params.valore+"'";
+//     dbMysql.query(stringa, (err,result)=>{
 
-        if (err) {console.log(err)}
-        res.send(result);
-    });
-    dbMysql.end();
-})
+//         if (err) {console.log(err)}
+//         res.send(result);
+//     });
+//     dbMysql.end();
+// })
 
-app.get('/credentials/:username/:password', (req, res) => {
-    dbMysql.connect();
-    const stringa = "select * from credenziali_utente where username = '" + req.params.username+"' and pw = '" + req.params.password+"'";
-    dbMysql.query(stringa, (err, result) => {
-        if (err) throw err
-        else res.send(result);
-    })
-    dbMysql.end();
-})
+// app.get('/credentials/:username/:password', (req, res) => {
+//     dbMysql.connect();
+//     const stringa = "select * from credenziali_utente where username = '" + req.params.username+"' and pw = '" + req.params.password+"'";
+//     dbMysql.query(stringa, (err, result) => {
+//         if (err) throw err
+//         else res.send(result);
+//     })
+// //     dbMysql.end();
+// // })
 
-/* API SCHEDATURA BENI */
+// /* API SCHEDATURA BENI */
 
-app.get('/letturaBene/:id', (req, res) => {
-    dbMysql.connect();
-    const stringa = "select * from beni_culturali where IdSchedaPk = '" + req.params.id+"'";
-    dbMysql.query(stringa, (err, result) => {
-        if (err) throw err
-        else res.send(result);
-    })
-    dbMysql.end();
-});
+// app.get('/letturaBene/:id', (req, res) => {
+//     dbMysql.connect();
+//     const stringa = "select * from beni_culturali where IdSchedaPk = '" + req.params.id+"'";
+//     dbMysql.query(stringa, (err, result) => {
+//         if (err) throw err
+//         else res.send(result);
+//     })
+//     dbMysql.end();
+// });
 
-app.post('/inserimentoBene/', jsonParser, (req, res) => {
-    dbMysql.connect();
-    const stringa = "insert into beni_culturali(data, luogo, nomeCaposquadra, nomeSchedatore, tipoOpera, descrizione, materiale, altezza, larghezza, profondità, note) VALUES('"+req.body.data+"','"+req.body.luogo+"','"+req.body.nomeResp+"','"+req.body.nomeSchedatore+"','"+req.body.tipoOpera+"','"+req.body.descrizione+"','"+req.body.materiale+"','"+req.body.altezza+"','"+req.body.larghezza+"','"+req.body.profondità+"','"+req.body.note+"')";
-    dbMysql.query(stringa, (err, result) => {
-        if (err) throw err
-        else res.send(result);
-    })
-    dbMysql.end();
-})
+// app.post('/inserimentoBene/', jsonParser, (req, res) => {
+//     dbMysql.connect();
+//     const stringa = "insert into beni_culturali(data, luogo, nomeCaposquadra, nomeSchedatore, tipoOpera, descrizione, materiale, altezza, larghezza, profondità, note) VALUES('"+req.body.data+"','"+req.body.luogo+"','"+req.body.nomeResp+"','"+req.body.nomeSchedatore+"','"+req.body.tipoOpera+"','"+req.body.descrizione+"','"+req.body.materiale+"','"+req.body.altezza+"','"+req.body.larghezza+"','"+req.body.profondità+"','"+req.body.note+"')";
+//     dbMysql.query(stringa, (err, result) => {
+//         if (err) throw err
+//         else res.send(result);
+//     })
+//     dbMysql.end();
+// })
 
 
 app.listen(port, ()=>{
